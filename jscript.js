f622faf30f43c70d6ea9169314ee609d9ea05eb8
Artem <mr.milkyshake@gmail.com>
2020-04-25 14:57:44 +0300

    Second Commit

--- jscript.js
+++ jscript.js
@@ -1,5 +1,5 @@
-let money = + "1000";//prompt("Ваш бюджет на месяц?");
-let time = "2020-04-25";//prompt("Введите дату в формате YYYY-MM-DD");
+let money = + prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
 let appData = {
     budget: money,
     timeData: time,
@@ -8,10 +8,10 @@ let appData = {
     income: [],
     savings: false
 };
-let a1 = "еда";//prompt("Введите обязательную статью расходов в этом месяце");
-let b1 = + "400";//prompt("Сколько это будет стоить?");
-let a2 = "аренда";//prompt("Введите обязательную статью расходов в этом месяце");
-let b2 = + "300";//prompt("Сколько это будет стоить?");
+let a1 = prompt("Введите обязательную статью расходов в этом месяце");
+let b1 = + prompt("Сколько это будет стоить?");
+let a2 = prompt("Введите обязательную статью расходов в этом месяце");
+let b2 = + prompt("Сколько это будет стоить?");
 appData.expenses[a1] = b1;
 appData.expenses[a2] = b2;
 let dailyBudget = (appData.budget - appData.expenses[a1] - appData.expenses[a2]) /30;
